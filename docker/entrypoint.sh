4a071bbe507679595f4ffaf089d9348e80850253
Jesus V <jveliz@almus.dev>
2025-11-19 15:01:52 -0400

    [DEL] Extras

--- docker/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-# Run the original Odoo entrypoint
-exec /usr/local/bin/odoo "$@"
--- utils/__init__.py
+++
@@ -1,4 +0,0 @@
-# Copyright 2024 tesote.com
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
-
-from . import sentry_config
